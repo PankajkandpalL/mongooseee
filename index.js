4946095afaddc202704c0eb1ca5887dd1d84b34f
PankajkandpalL <pankajkandpal5602@gmail.com>
2024-04-18 14:25:03 +0530

    configured eslint of airbnb base

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const { userRouter } = require("./routes/user.route");
-const { connectionToMongoAtlas } = require("./config/dbConfig");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { userRouter } = require('./routes/user.route');
+const { connectionToMongoAtlas } = require('./config/dbConfig');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req,res)=>{
-    res.send("Server is up!")
-})
+app.get('/', (req, res) => {
+  res.send('Server is up!');
+});
 
-app.use("/user", userRouter);
+app.use('/user', userRouter);
 
-app.listen(8080, async(req,res)=>{
-    await connectionToMongoAtlas();
-    console.log("Server is running");
-})
-
-
-
+app.listen(8080, async(req, res) => {
+  await connectionToMongoAtlas();
+  console.log('Server is running');
+});
